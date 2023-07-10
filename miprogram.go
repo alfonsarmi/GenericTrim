c5a634c85f6c75f6aaf9ffea6d6109e5fa7b2c7b
Alfonso Sarmiento Ráez <alfonsarmi@gmail.com>
2023-07-10 14:52:41 +0200

    Another test

--- miprogram.go
+++ miprogram.go
@@ -5,7 +5,10 @@ import "fmt"
 func main() {
    							 fmt.Println("¡Hola, mundo!")
 }
-
+//this is a test
+         
+          
+         
 func main2() {             
     fmt.Println("¡Hola,                     mundo!")                         
 			}
