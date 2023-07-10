b09b2cc49bac4fc200c0d14f1b1ad7b490b4e073
Alfonso Sarmiento Ráez <alfonsarmi@gmail.com>
2023-07-10 14:19:51 +0200

    fix

---
+++ miprogram.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+   							 fmt.Println("¡Hola, mundo!")
+}
+
+func main2() {             
+    fmt.Println("¡Hola,                     mundo!")                         
+			}
