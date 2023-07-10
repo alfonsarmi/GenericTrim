4a6aac6c28230065bb73fe59f0c72819d8bc36f2
Alfonso Sarmiento Ráez <alfonsarmi@gmail.com>
2023-07-10 15:18:53 +0200

    Fix

--- miprogram.go
+++ miprogram.go
@@ -5,10 +5,10 @@ import "fmt"
 func main() {
    							 fmt.Println("¡Hola, mundo!")
 }
-//this is a test
-         
-          
-         
+//this is a test asdfasdf asdf sadf                                                         
+                                   sadfasdf 
+          sdfsadf                            
+          sfdasdfsdaf                                    
 func main2() {             
-    fmt.Println("¡Hola,                     mundo!")                         
+    fmt.Println("¡Hola,                     mundo!")                                                                                                                                
 			}
