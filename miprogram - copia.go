b85fe9d5ca9aea6fe3fb83e89f7f99085c0eb3b6
Alfonso Sarmiento Ráez <alfonsarmi@gmail.com>
2023-07-11 08:56:10 +0200

    test

---
+++ miprogram - copia.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+   							 fmt.Println("¡Hola, mundo!")
+}
+//this is a test asdfasdf asdf sadf                                                         
+                                   sadfasdf 
+          sdfsadf                            
+          sfdasdfsdaf                                    
+func main2() {             
+    fmt.Println("¡Hola,                     mundo!")                                                                                                                                
+			}
